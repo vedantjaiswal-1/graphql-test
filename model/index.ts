21c5b12247efcc03127c4109bba160d7cbfd6df2
vedant <vedantjaiswal@gmail.com>
2022-04-19 17:31:18 +0530

    delete files

--- model/index.ts
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    
-    User: require('./user'),
-    Post: require('./post'),
-    Comment: require('./comment')
-}
--- util/auth.ts
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const createJwtToken = (user: any) => {
-  return jwt.sign({ user }, "121215", {
-    expiresIn: "30 days"
-  });
-};
-
-module.exports = { createJwtToken };
