741016af15392c2c7bc68450e5bfdfc2a0286625
vedant <vedantjaiswal@gmail.com>
2022-04-19 16:22:06 +0530

    first commit

---
+++ middleware/auth.ts
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+const authenticate = async (req: any, res: any, next: any) => {
+  const token = req?.headers?.authorization?.split(" ")[1] || "";
+
+  try {
+    const verified = jwt.verify(token, "121215");
+    // const decoded = jwt.decode(token, process.env.JWT_SECRET)
+    req.verifiedUser = verified?.user;
+    next();
+  } catch (err) {
+    console.log("Verification failed!", err);
+    next();
+  }
+};
+
+module.exports = { authenticate };
---
+++ model/index.ts
@@ -0,0 +1,6 @@
+module.exports = {
+    
+    User: require('./user'),
+    Post: require('./post'),
+    Comment: require('./comment')
+}
